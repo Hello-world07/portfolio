83eb143300be6ccca43aa376595ef4b2ab57d518
Pranith Konda <pranithkonda@Praniths-MacBook-Air.local>
2025-05-30 13:34:36 +0530

    fixed tailwindcss logo

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -23,7 +23,7 @@ const Skills = () => {
         { name: "HTML5", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
         { name: "CSS3", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
         { name: "React.js", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-        { name: "Tailwind CSS", logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" },
+        { name: "Tailwind CSS", logo: "https://cdn.simpleicons.org/tailwindcss/06B6D4" },
       ],
       bgClass: "bg-blue-50 dark:bg-gray-700",
     },
