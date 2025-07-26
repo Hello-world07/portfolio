10482ff483727058fcc01c23b8b2516988db7b27
pranith <pranithkondakp@gmail.com>
2025-07-26 10:51:25 +0530

    added resume

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -83,7 +83,7 @@ const Navbar = () => {
               <span className="relative z-10">Contact Me</span>
             </Link>
             <a
-              href="/pranith.pdf"
+              href="/resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
               download
